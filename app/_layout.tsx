1b8e61aa100b063a664d7ef1c79e6521a26d94d0
PETIT <armand_petit@outlook.fr>
2025-04-16 07:30:30 +0200

    add StatusBar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,14 @@
 import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 
 export default function RootLayout() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <BottomSheetModalProvider>
         <Stack screenOptions={{ headerShown: false }} />
+        <StatusBar style="dark" />
       </BottomSheetModalProvider>
     </GestureHandlerRootView>
   );
